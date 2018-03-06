765e91f788b55fdbc4ce8ea272b0e49e38cd569a
Tyler Young <tyleryoung210@gmail.com>
2018-03-06 18:11:13 -0500

    Adding the slidetoggle to the sections

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,15 @@
 $("#hamburger").on("click", function() {
 	$("#navbar-hidden").slideToggle(300);
 });
+
+$("#project_header").on("click", function() {
+	$("#project_content").slideToggle(600);
+});
+
+$("#involvement_header").on("click", function() {
+	$("#involvement_content").slideToggle(600);
+});
+
+$("#favorites_header").on("click", function() {
+	$("#favorites_content").slideToggle(600);
+});
